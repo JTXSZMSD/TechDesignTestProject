57407b7f95c1f42d7bd138ca75ff8b9587cdbc24
JTXSZMSD <91826944+JTXSZMSD@users.noreply.github.com>
2023-08-17 11:56:31 +0300

    Scene 1 final

--- Assets/Scripts/Buddy/Buddy_collision_manager.cs
+++ Assets/Scripts/Buddy/Buddy_collision_manager.cs
@@ -5,10 +5,13 @@ public class Buddy_collision_manager : MonoBehaviour
     private Animator anim;
     private Buddy_moveset ms;
 
+    private AudioSource audio;
+
     void Start()
     {
         anim = GetComponent<Animator>();
         ms = GetComponent<Buddy_moveset>();
+        audio = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -17,6 +20,12 @@ public class Buddy_collision_manager : MonoBehaviour
         {
             anim.SetBool("Is_finished", true);
             ms.enabled = false;
+            Invoke("audio_enable", 0.5f);
         }
+    }
+
+    private void audio_enable()
+    {
+        audio.enabled = true;
     }
 }
